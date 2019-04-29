1df6e6f48604056e293fad042a9de6483ae0cea7
taslimy <taslimer@gmail.com>
2019-04-29 15:57:31 -0400

    Project set up completed and running.

---
+++ instagram-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <h1>Testing to make sure everything displays correctly</h1>
+      </>
+    </div>
+  );
+}
+
+export default App;
